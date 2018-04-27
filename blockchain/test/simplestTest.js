fbb469c939004d688e4381e602e6d6fb05994ca7
Kyle <kjdchapman@gmail.com>
2018-04-27 15:06:42 +0100

    new()->deployed() to stop test state pollution

--- blockchain/test/simplestTest.js
+++ blockchain/test/simplestTest.js
@@ -2,30 +2,22 @@ var Players = artifacts.require("./Players.sol");
 
 contract('Player', function(accounts) {
   it("can join the game", function() {
-    return Players.deployed().then(function(instance) {
+    return Players.new().then(function(instance) {
       instance.join("bob");
     });
   });
-});
 
-// If this test is uncommented, then the tests pass.
-// If it's commented, the tests fail.
-// wtf.
-;
-// contract('Player', function(accounts) {
-//   it("can find my player name", function() {
-//     return Players.deployed().then(function(instance) {
-//       instance.join("bob");
-//       return instance.getCurrentPlayer().then(function(result) {
-//         assert.equal(result, "bob");
-//       });
-//     });
-//   });
-// });
+  it("can find my player name", function() {
+    return Players.new().then(function(instance) {
+      instance.join("bob");
+      return instance.getCurrentPlayer().then(function(result) {
+        assert.equal(result, "bob");
+      });
+    });
+  });
 
-contract('Player', function(accounts) {
   it("returns null if I haven't joined", function() {
-    return Players.deployed().then(function(instance) {
+    return Players.new().then(function(instance) {
       return instance.getCurrentPlayer().then(function(result) {
         assert.equal(result, '');
       });
