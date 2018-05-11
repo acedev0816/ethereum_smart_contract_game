47ea92775f08f01c43630b6d7a0db579b9f76f02
Kyle <kjdchapman@gmail.com>
2018-05-11 17:23:36 +0100

    WIP more granular player data

--- blockchain/contracts/Players.sol
+++ blockchain/contracts/Players.sol
@@ -2,22 +2,28 @@ pragma solidity ^0.4.18;
 
 contract Players {
   struct Player {
-    string name;
     uint16 balance;
   }
 
   mapping(address => Player) addressToPlayer;
+  address[] players;
 
-  function join (string name) public payable costs(1){
+  function join () public payable costs(1){
     addressToPlayer[msg.sender] = Player(
-      name,
       uint16(1500)
     );
+
+    players.push(msg.sender);
   }
 
-  function getCurrentPlayer () public view returns (string, uint16) {
-    Player memory player = addressToPlayer[msg.sender];
-    return(player.name, player.balance);
+  function getPlayers () public view returns (address[]) {
+    return players;
+  }
+
+  function getBalance (address address) public view returns (uint16) {
+    player = addressToPlayer[address];
+    if player return player.balance;
+    return 0;
   }
 
   modifier costs(uint price) {
--- blockchain/test/PlayerTest.js
+++ blockchain/test/PlayerTest.js
@@ -3,39 +3,13 @@ var Players = artifacts.require("./Players.sol");
 contract('Player', function(accounts) {
   it("can join the game", function() {
     return Players.new().then(function(instance) {
-      instance.join("bob", { value: 1 });
-    });
-  });
-
-  it("can find my player name", function() {
-    return Players.new().then(function(instance) {
-      instance.join("bob", { value: 1 });
-      return instance.getCurrentPlayer().then(function(result) {
-        assert.equal(result[0], "bob");
-      });
-    });
-  });
-
-  it("can find my player balance", function() {
-    return Players.new().then(function(instance) {
-      instance.join("bob", { value: 1 });
-      return instance.getCurrentPlayer().then(function(result) {
-        assert.equal(result, 1500);
-      });
-    });
-  });
-
-  it("returns null if I haven't joined", function() {
-    return Players.new().then(function(instance) {
-      return instance.getCurrentPlayer().then(function(result) {
-        assert.equal(result, '');
-      });
+      instance.join({ value: 1 });
     });
   });
 
   it("requires at least 1 ETH", function(){
     return Players.new().then(function(instance) {
-      instance.join("bob", { value: 0.999 }).catch(function(result) {
+      instance.join({ value: 0.999 }).catch(function(result) {
         assert.ok(true);
       });
     });
@@ -43,8 +17,36 @@ contract('Player', function(accounts) {
 
   it("requires no more than 1 ETH", function(){
     return Players.new().then(function(instance) {
-      instance.join("bob", { value: 1.001 }).catch(function(result) {
+      instance.join({ value: 1.001 }).catch(function(result) {
         assert.ok(true);
+      });
+    });
+  });
+
+  it("can get all the player addresses", function() {
+    return Players.new().then(function(instance) {
+      instance.join({ value: 1, from: accounts[0] });
+      instance.join({ value: 1, from: accounts[1] });
+
+      return instance.getPlayers().then(function(result) {
+        expect(result).to.deep.equal([ accounts[0], accounts[1] ]);
+      });
+    });
+  });
+
+  it("can find my player balance", function() {
+    return Players.new().then(function(instance) {
+      instance.join({ value: 1 });
+      return instance.getBalance(accounts[0]).then(function(result) {
+        assert.equal(result, 1500);
+      });
+    });
+  });
+
+  xit("returns null if I haven't joined", function() {
+    return Players.new().then(function(instance) {
+      return instance.getCurrentPlayer().then(function(result) {
+        assert.equal(result, '');
       });
     });
   });
